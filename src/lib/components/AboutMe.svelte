dc9107ff13a9e045c909c86ccf423692feca75e8
Mikuláš Vojíř <vojir.mikulas@gmail.com>
2023-11-07 23:59:24 +0100

    feature/1 - basic styling

---
+++ src/lib/components/AboutMe.svelte
@@ -0,0 +1,7 @@
+<section class="relative flex flex-col">
+    <span class="text-neutral-600 absolute -translate-x-full pr-2 mobile:static mobile:my-2 mobile:translate-x-0">About Me</span>
+    <p class="text-neutral-100"> I'm a 21-year-old Full Stack developer, specializing in React and SvelteKit for the
+        frontend and Node.js
+        for the backend. 💻 I'm passionate about clean code and continually strive for improvement. 😮‍💨 I enjoy
+        learning and am dedicated to achieving excellent results in my field. 🚀</p>
+</section>
