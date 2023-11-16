49faa0f326641abf36b2bd7c58d160f22b3c63a7
Mikuláš Vojíř <mikulas.vojir@r2b2.cz>
2023-11-16 14:35:09 +0100

    gam (will be reverted)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,8 @@
     import "../app.css"
 </script>
 
+<svelte:head>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6775349100535553"
+            crossorigin="anonymous"></script>
+</svelte:head>
 <slot />
